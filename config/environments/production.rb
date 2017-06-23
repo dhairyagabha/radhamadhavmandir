dcd0737fd6e09e827c39426efa56e2b35f17b1f6
Dhairya Gabhawala <dhairya@rev365.com>
2017-06-23 08:55:10 -0500

    email

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -63,11 +63,11 @@ Rails.application.configure do
   config.action_mailer.raise_delivery_errors = true
 
   config.action_mailer.perform_caching = false
-  config.action_mailer.default_url_options = {host: ENV['DOMAIN']}
+  config.action_mailer.default_url_options = {host: ENV['APP_DOMAIN']}
   config.action_mailer.delivery_method = :smtp
 
   config.action_mailer.smtp_settings = {
-      address: 'smtp.sendgrid.net',
+      address: 'smtp.gmail.com',
       port: 587,
       domain: ENV['DOMAIN'],
       user_name: ENV['EMAIL'],
